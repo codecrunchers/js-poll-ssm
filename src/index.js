f5345e67680354604ec3ea6b3a27d55969df2f70
Al Ryan <alan@planet9energy.com>
2017-11-15 14:55:03 +0000

    Logger Working

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 'use strict'
 const pino = require('pino')
 const pretty = pino.pretty()
-const logLevel = process.env.LOG_LEVEL
+const logLevel = process.env.LOG_LEVEL == undefined ? 'debug' : process.env.LOG_LEVEL
 
 pretty.pipe(process.stdout)
 const logger = pino({
