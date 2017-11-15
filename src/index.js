39fc452e64e08d78b862a4340864e8d0d10c315b
Al Ryan <alan@planet9energy.com>
2017-11-15 14:22:55 +0000

    Basic Skel App

--- src/index.js
+++ src/index.js
@@ -1,27 +1,15 @@
 'use strict'
 const pino = require('pino')
-const yargs = require('yargs')
-const argv = yargs
 const pretty = pino.pretty()
 const logLevel = process.env.LOG_LEVEL
 
 pretty.pipe(process.stdout)
 const logger = pino({
-    name: 'webhook-tunnel',
+    name: 'pollSsm',
     safe: true,
     level: logLevel
 }, pretty)
 
-logger.debug({
-    runtime: process.argv[0],
-    script: process.argv[1],
-    arguments: argv
-}, 'Initialising')
-
-const pollSsm = require('./pollSsm')
-
-try {
-    pollSsm('test', 10)
-} catch (err) {
-    logger.error(err, 'Polling of SSM Failed')
-}
+logger.debug('Initialising')
+require('./pollSsm')
+logger.debug('Done')
