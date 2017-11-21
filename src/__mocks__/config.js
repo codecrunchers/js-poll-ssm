6dc30961d436fd068129bca7db2ad2e8c98ae693
Al Ryan <alan@planet9energy.com>
2017-11-21 01:21:58 +0000

    ShAky TeStS

--- src/__mocks__/config.js
+++ src/__mocks__/config.js
@@ -1,5 +1,5 @@
 const config = {
-	providerType: "none"
+	providerType: 'none'
 }
 
 module.exports = config
--- src/__tests__/store.providerfactory.test.js
+++ src/__tests__/store.providerfactory.test.js
@@ -1,10 +1,32 @@
 /* eslint-env jest */
 
-jest.mock('config')
-const ParamProvider = require('src/store.providerfactory.js').create({})
+//jest.mock('logger')
+jest.dontMock('src/store.providerfactory.js');
+
 
 describe('#create()', () => {
-    it('should return null for anything but aws|redis', () => {
-        expect(ParamProvider()).toBeNull
+    beforeEach(() => {
+        jest.resetModules();
+
+    });
+
+    it('should return an AWS object for \'aws\'', () => {
+        expect(
+            () => {
+                jest.setMock('config', {
+                    providerType: 'asdasdasdasadsasd'
+                })
+				null
+            }).not.toBeNull()
     })
+
+
+    it('should return null for anything but aws|redis', () => {
+        expect(
+            () => {
+                let config = jest.mock('config')
+                var r = require('src/store.providerfactory.js').create({})
+            }).toThrowError('Invalid Provider Specified: none');
+    })
+
 })
