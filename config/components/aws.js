4121542c8dcc1242a7660997259fe712cacc7141
Al Ryan <alan@planet9energy.com>
2017-11-22 14:03:28 +0000

    more env var consistency

--- config/components/aws.js
+++ config/components/aws.js
@@ -4,8 +4,8 @@ const DEFAULT_TIMEOUT = 30000
 
 const config = {
     providerType: process.env.PROVIDER_TYPE,
-    aws_access_key_id: process.env.AWS_ACCESS_KEY_ID,
-    aws_secret_access_key: process.env.AWS_SECRET_ACCESS_KEY,
+    awsAccessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    awsSecretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     region: process.env.AWS_REGION,
     pollTime: process.env.POLL_TIME === undefined ? DEFAULT_TIMEOUT : parseInt(process.env.POLL_TIME),
     keyPrefix: process.env.KEY_PREFIX === undefined ? EMPTY_STRING : process.env.KEY_PREFIX,
--- models/aws/aws.js
+++ models/aws/aws.js
@@ -1,11 +1,11 @@
 'use strict'
+const AWS = require('aws-sdk');
 const config = require('../../config')
 const logger = require('../../logger')
-var AWS = require('aws-sdk');
 
 const aws_secure_config = {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: config.awsAccessKeyId,
+    secretAccessKey: config.awsSecretAccessKey,
     region: config.region
 }
 module.exports = new AWS.SSM(aws_secure_config)
